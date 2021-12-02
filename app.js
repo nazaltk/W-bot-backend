c3d416406eb42ea9b574abc00d3b1ce381ceb1ff
Glitch (atp-bot) <none>
2021-12-02 12:28:59 +0000

    🐯🍎 Checkpoint ./app.js:39096088/1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { phoneNumberFormatter } = require('./helpers/formatter');
 const fileUpload = require('express-fileupload');
 const axios = require('axios');
 const mime = require('mime-types');
-
+ 
 const TEMPLATE_URL = process.env.TEMPLATE_URL;
 const port = process.env.PORT || 8000;
 
